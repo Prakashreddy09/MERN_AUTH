b9d7cfe5b8754861a6a6a2f73da5d1e5e2848ba2
Prakashreddy09 <pguru2@gitam.in>
2024-04-13 15:04:54 +0530

    Create Header Component, And Connect pages using Link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import About from './Pages/About'
 import Signin from './Pages/Signin'
 import Signup from './Pages/Signup'
 import Profile from './Pages/Profile'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
